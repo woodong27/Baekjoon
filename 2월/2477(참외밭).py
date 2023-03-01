3e5d8732ad85efb97b7b86f5a725b45020bdbc99
woodong27 <cdw5548@gmail.com>
2023-03-02 08:54:15 +0900

    3월추가

---
+++ 2월/2477(참외밭).py
@@ -0,0 +1,6 @@
+K=int(input())
+
+move=[]
+maxw,maxh=0,0
+for i in range(6):
+    cardinal,distance=map(int,input().split())
---
+++ 2월/2527(직사각형).py
@@ -0,0 +1,12 @@
+for _ in range(4):
+    j1,i1,j2,i2,jj1,ii1,jj2,ii2=map(int,input().split())
+
+    if
+
+    print('첫사각형')
+    print(range(i1,i2))
+    print(range(j1,j2))
+    print('두번째사각형')
+    print(range(ii1,ii2))
+    print(range(jj1,jj2))
+    print()
